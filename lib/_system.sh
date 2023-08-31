4183e9b5d5acea3b61e6a8157567970a54524c2a
OiEE Technology & Marketing <123946880+oieeteckmark@users.noreply.github.com>
2023-08-31 20:33:15 -0300

    Add files via upload

--- lib/_system.sh
+++ lib/_system.sh
@@ -284,10 +284,10 @@ system_node_install() {
   sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR=16 nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   sudo apt-get update && sudo apt-get install nodejs -y
   sleep 2
-  npm install -g npm@latest
+  curl http://npmjs.org/install.sh | sudo sh
   sleep 2
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
