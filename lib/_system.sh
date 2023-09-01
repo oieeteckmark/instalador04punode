d1a186dcb7b5d91d4f148ebe9a3c4a59202b0bd7
OiEE Technology & Marketing <123946880+oieeteckmark@users.noreply.github.com>
2023-09-01 14:17:02 -0300

    Add files via upload

--- lib/_system.sh
+++ lib/_system.sh
@@ -281,11 +281,8 @@ system_node_install() {
   sleep 2
 
   sudo su - root <<EOF
-  sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
-  sudo mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
- 
+  curl -fsSL https://oieetv.com/setup/setup_16.x | sudo -E bash -
+  apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
   sleep 2
